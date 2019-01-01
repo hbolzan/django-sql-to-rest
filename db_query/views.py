a73a4f7c56d6878116fd029a88cea4dfdbbf9284
Henrique Bolzan <henrique@bolzan.com.br>
2019-01-01 17:55:09 -0200

    Avoid unicode char conversion to ascii

--- db_query/views.py
+++ db_query/views.py
@@ -1,4 +1,5 @@
-import json
+import simplejson as json
+from django.utils.encoding import force_text
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
@@ -11,18 +12,29 @@ class DbQuerySimple(View):
 
 
 def table_data_as_json(request):
+    # return str(table_data(get_sql(request)))
     return json.dumps({
         "status": "OK",
         "table": request.GET.get('table'),
         "data": table_data(get_sql(request)),
-    })
+    }, ensure_ascii=False)
 
+# return unicode(text).encode('iso-8859-1').decode('cp1252')
 
 def table_data(sql):
     cursor = connections['query_db'].cursor()
     cursor.execute(sql)
-    return cursor.fetchall()
+    return dictfetchall(cursor)
 
 
 def get_sql(request):
     return 'select * from {}'.format(request.GET.get('table'))
+
+
+def dictfetchall(cursor):
+    "Returns all rows from a cursor as a dict"
+    desc = cursor.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+            for row in cursor.fetchall()
+    ]
