118e3145e0a9949fe22538674e5f7da2025118be
Henrique Bolzan <henrique@bolzan.com.br>
2019-09-16 11:17:38 -0300

    Better error handling for services view

--- services/views.py
+++ services/views.py
@@ -2,8 +2,8 @@ import os
 import json
 from django.views import View
 from django.http import HttpResponse
-from dstr_common_lib.controller import handle_service_request
-from dstr_common_lib.consts import HTTP_STATUS_OK
+from dstr_common_lib.controller import handle_service_request, build_response_body
+from dstr_common_lib.consts import HTTP_STATUS_OK, HTTP_STATUS_ERROR
 
 
 class Services(View):
@@ -17,18 +17,30 @@ class Services(View):
 
     def get(self, request, service_name, method, param=None):
         kwargs = {k: v for k, v in request.GET.items()}
-        service_response = handle_service_request(service_name, method, param, **kwargs)
-        return HttpResponse(
-            content=json.dumps(service_response.get("body"), ensure_ascii=False),
-            content_type="application/json",
-            status=service_response.get("http_status", HTTP_STATUS_OK),
+        return self.response_body(
+            lambda: handle_service_request(service_name, method, param, **kwargs)
         )
 
     def post(self, request, service_name, method):
         request_data = json.loads(request.body)
-        service_response = handle_service_request(service_name, method, request_data)
+        return self.response_body(
+            lambda: handle_service_request(service_name, method, request_data)
+        )
+
+    def response_body(self, service_fn):
+        try:
+            return self.http_response(HTTP_STATUS_OK, service_fn())
+        except Exception as e:
+            return self.http_response(
+                HTTP_STATUS_ERROR, self.error_response(e.args[0])
+            )
+
+    def error_response(self, message):
+        return {"body": build_response_body(HTTP_STATUS_ERROR, message, message)}
+
+    def http_response(self, status, response):
         return HttpResponse(
-            content=json.dumps(service_response.get("body"), ensure_ascii=False),
+            content=json.dumps(response.get("body"), ensure_ascii=False),
             content_type="application/json",
-            status=service_response.get("http_status", HTTP_STATUS_OK),
+            status=response.get("http_status", status),
         )
