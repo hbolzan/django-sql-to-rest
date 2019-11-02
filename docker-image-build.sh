1133fa6f4013c231b853222eed767e2db7318c6a
Henrique Bolzan <henrique@bolzan.com.br>
2019-11-02 08:25:56 -0300

    Autoincrement version builder

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION=$1
-echo "# Version: ${VERSION}" > version.txt
+./inc_version.py
+VERSION=`cat VERSION`
 
 docker build --build-arg VERSION=$VERSION --build-arg USER=$DOCKER_USER_ID \
        -t "${DOCKER_USER_ID}/django-sql-to-rest:latest" \
