008f2d726fc7b758e45d24eb2e32ae605f194947
Henrique Bolzan <henrique@bolzan.com.br>
2020-07-24 22:10:45 -0300

    Fix timezone error

--- django_sql_to_rest/settings.py
+++ django_sql_to_rest/settings.py
@@ -156,13 +156,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
